af8b0e4ca8f824d7ab033587f4c6e51977bbc3ab
Hugo Junior <hjunioreng@gmail.com>
2023-01-28 10:05:11 -0300

    fix: ajustes nos filtros de categoria e layout

--- src/components/Category.js
+++ src/components/Category.js
@@ -1,22 +1,14 @@
-import styled from "styled-components"
 import Product from "./Product"
 
-export default function Category({name, price, category, unity, picture,  }){
+export default function Category({name, price, category, unity, picture, all}){
 
-
+    
 return (
 
 <div>
 <h3> {category}</h3>
-
-
+{all.map( p => (  <Product name = {p.name} price ={p.price} category={p.category} unity = {p.unity} picture ={p.picture} /> ))}
 </div>
 
 )
 }
-
-
-const Container = styled.ul`
-display:flex;
-flex-direction:column;
-`
